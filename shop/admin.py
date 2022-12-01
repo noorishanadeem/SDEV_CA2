240160fe9d88b6a1d3873976176b992a4bc25ae1
Noor Isha Nadeem <email_I_use_for_github@gmail.com>
2022-12-01 13:20:39 +0000

    isha

--- shop/admin.py
+++ shop/admin.py
@@ -1,3 +1,15 @@
 from django.contrib import admin
+from .models import Type, Product
 
 # Register your models here.
+class TypeAdmin(admin.ModelAdmin):
+    list_display = ['name']
+
+admin.site.register(Type, TypeAdmin)
+
+class ProductAdmin(admin.ModelAdmin):
+    list_display = ['name', 'price', 'description', 'type', 'stock','available', 'created', 'updated']
+    list_editable = ['price', 'stock', 'available']
+    list_per_page = 20
+
+admin.site.register(Product, ProductAdmin)
--- shop_project/urls.py
+++ shop_project/urls.py
@@ -1,4 +1,4 @@
-"""ShopProject URL Configuration
+"""shop_project URL Configuration
 
 The `urlpatterns` list routes URLs to views. For more information please see:
     https://docs.djangoproject.com/en/4.1/topics/http/urls/
@@ -15,7 +15,9 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from django.conf import settings
+from django.conf.urls.static import static
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-]
+] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
