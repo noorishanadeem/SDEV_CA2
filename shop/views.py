bb541b1432041bea726383c73c465ac7e0ddbcdd
Noor Isha Nadeem <email_I_use_for_github@gmail.com>
2022-12-05 11:43:55 +0000

    isha

--- shop/views.py
+++ shop/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render, get_object_or_404
 from .models import Type, Product
+from django.core.paginator import Paginator, EmptyPage, InvalidPage
 
 # Create your views here.
 def prod_list(request, type_id=None):
@@ -8,6 +9,17 @@ def prod_list(request, type_id=None):
     if type_id:
         type = get_object_or_404(Type, id=type_id)
         products = Product.objects.filter(type=type, available=True)
+
+        paginator = Paginator(products, 6)
+        try:
+            page = int(request.GET.get('page', '1'))
+        except: 
+            page = 1
+        try:
+            products = paginator.page(page)
+        except (EmptyPage,InvalidPage):
+            products = paginator.page(paginator.num_pages)
+
     return render(request, 'shop/type.html', {'type':type, 'prods':products})
 
 def product_detail(request, type_id, product_id):
